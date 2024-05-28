ac483f4a324fdae9e42ef9a5007c822384a69a75
Just0guy <alazzanimoh@gmail.com>
2024-05-28 23:47:38 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,11 +1,11 @@
-first = input("Введите первое число: ")
-second = input("Введите второе число: ")
-third = input("Введите Третье число: ")
-if first == second == third :
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите Третье число: "))
+if first == second == third:
     print(3)
-elif first == second or second == third or first == third :
+elif first == second or second == third or first == third:
     print(2)
-elif first != second and second != third and first != third :
+elif first != second and second != third and first != third:
     print(0)
 else:
     print("Ой братка чет не шариш ты за матан")
