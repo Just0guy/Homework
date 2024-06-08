7bd3a913847b77dae01c0cdf84a1164448f792ef
Just0guy <alazzanimoh@gmail.com>
2024-06-08 21:27:35 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,10 @@
+def generator(n):
+    password = ('')
+    for i in range(1, n - 1):
+        for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+for n in range(3, 21):
+    password = generator(n)
+    print(f"{n} - {password}")
